398604b2cbac9820ac48acf4b985b85992ce317b
Toobasultan <167599051+Toobasultan@users.noreply.github.com>
2024-05-20 08:08:39 -0700

    Add files via upload

---
+++ javascript/chap1.js
@@ -0,0 +1,6 @@
+alert("welcome to our website")
+alert("Error!Please enter a valid a password")
+alert("Welcome to JS Land...\nHappy coding!")
+alert("Welcome to JS Land");
+alert("Happy coding!")
+console.log("alert Hello...I can run JS through my web browser's console")
